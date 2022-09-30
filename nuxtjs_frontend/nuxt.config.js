916549846ec06a843b3d6831c2ce912404c0f454
ikuto-asako <m131932@gmail.com>
2022-09-30 17:34:24 +0900

    nuxt.config設定値変更

--- nuxtjs_frontend/nuxt.config.js
+++ nuxtjs_frontend/nuxt.config.js
@@ -57,7 +57,7 @@ export default {
     strategies: {
       laravelApi: {
         provider: "laravel/sanctum",
-        url: "http://localhost",
+        url: "http://ready-to-session.com",
         endpoints: {
           login: { url: "/login", method: "post" },
           logout: { url: "/logout", method: "post" },
@@ -70,7 +70,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: "http://localhost",
+    baseURL: "http://ready-to-session.com",
     credentials: true,
   },
 
