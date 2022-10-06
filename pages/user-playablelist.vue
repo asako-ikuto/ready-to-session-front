fc35abf0dbd26708b945441de7623bf1ac5aabbd
ikuto-asako <m131932@gmail.com>
2022-10-06 12:28:37 +0900

    user-playablelist.vueのsp追加日削除

--- pages/user-playablelist.vue
+++ pages/user-playablelist.vue
@@ -36,11 +36,6 @@
                         song.artist_name
                       }}</v-list-item-subtitle>
                     </v-list-item-content>
-                    <v-list-item-action>
-                      <v-list-item-action-text>{{
-                        $dateFns.format(song.updated_at, "yyyy.MM.dd")
-                      }}</v-list-item-action-text>
-                    </v-list-item-action>
                   </v-list-item>
                 </template>
               </v-col>
